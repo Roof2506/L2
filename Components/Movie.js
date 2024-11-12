64af1cb38836ee12a0f498b5ee5cc8a9a36e0bb3
Roof2506 <Atticuslimtzebin@gmail.com>
2024-11-12 14:29:05 +0800

    Update

---
+++ Components/Movie.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, Image, ScrollView} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome6';
+
+
+
+const Movie = ({title, year,icon_name,poster})=> {
+    return (
+        <View>
+            <Image source={poster} style={{width:400, height:500}}/>
+            <Text>
+                {title} {year}.
+            </Text>
+            <Icon name={icon_name} size={50} color="#B23B23"/>
+        </View>
+    );
+};
+const AllMovies = () => {
+    return  (
+        <ScrollView>
+            <Movie title="Doctor Sleep" year={2019} icon_name='skull' poster={require('../img/doctor-sleep.jpg')}/>
+            <Movie title="Midway" year={2020} icon_name='person-rifle' poster={require('../img/midway.jpg')}/>
+        </ScrollView>
+    )
+}
+export default AllMovies;
